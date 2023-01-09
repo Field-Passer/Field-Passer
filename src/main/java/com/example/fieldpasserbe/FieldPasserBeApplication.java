dbe409989324ed347dffd9a0fff5be88f6220d74
cocoiscat <demy176@naver.com>
2023-01-09 15:37:56 +0900

    Initial Commit

---
+++ src/main/java/com/example/fieldpasserbe/FieldPasserBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fieldpasserbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldPasserBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldPasserBeApplication.class, args);
+    }
+
+}
