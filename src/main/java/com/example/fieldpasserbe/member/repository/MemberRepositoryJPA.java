79f69b2bd05ea6656c82d4c7d11e0b707f74f956
cocoiscat <demy176@naver.com>
2023-01-29 01:26:12 +0900

    feat: MemberRepository 개발
    id로 유저 찾기, 이메일로 유저 찾기

--- src/main/java/com/example/fieldpasserbe/member/repository/MemberRepositoryJPA.java
+++ src/main/java/com/example/fieldpasserbe/member/repository/MemberRepositoryJPA.java
@@ -2,6 +2,8 @@ package com.example.fieldpasserbe.member.repository;
 
 import com.example.fieldpasserbe.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,8 +11,11 @@ import java.util.Optional;
 @Repository
 public interface MemberRepositoryJPA extends JpaRepository<Member, Integer> {
 
+    //id로 유저 찾기
     Optional<Member> findMemberById(int id);
 
+    //이메일로 유저 찾기
     Optional<Member> findMemberByEmail(String email);
+
 
 }
