7d21f18c30a73849785d064d6078304e3eff9f78
cocoiscat <demy176@naver.com>
2023-01-30 00:05:55 +0900

    feat: 회원 목록 조회

--- src/main/java/com/example/fieldpasserbe/admin/service/AdminService.java
+++ src/main/java/com/example/fieldpasserbe/admin/service/AdminService.java
@@ -2,9 +2,11 @@ package com.example.fieldpasserbe.admin.service;
 
 import com.example.fieldpasserbe.admin.dto.AdminLoginRequestDTO;
 import com.example.fieldpasserbe.admin.vo.AdminLoginVO;
+import com.example.fieldpasserbe.admin.vo.MemberListVO;
 
 import javax.servlet.http.HttpSession;
 
 public interface AdminService {
     AdminLoginVO adminLogin(AdminLoginRequestDTO admin, HttpSession session) throws Exception;
+    MemberListVO lookUpmembers() throws Exception;
 }
--- src/main/java/com/example/fieldpasserbe/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/example/fieldpasserbe/admin/service/impl/AdminServiceImpl.java
@@ -2,14 +2,20 @@ package com.example.fieldpasserbe.admin.service.impl;
 
 import com.example.fieldpasserbe.admin.dto.AdminLoginRequestDTO;
 import com.example.fieldpasserbe.admin.dto.AdminLoginResponceDTO;
+import com.example.fieldpasserbe.admin.dto.MemberListDTO;
 import com.example.fieldpasserbe.admin.service.AdminService;
 import com.example.fieldpasserbe.admin.vo.AdminLoginVO;
+import com.example.fieldpasserbe.admin.vo.MemberListVO;
+import com.example.fieldpasserbe.board.service.BoardService;
 import com.example.fieldpasserbe.member.entity.Member;
 import com.example.fieldpasserbe.member.service.MemberService;
+import com.example.fieldpasserbe.support.service.PunishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,8 +24,19 @@ public class AdminServiceImpl implements AdminService {
 
     @Autowired
     private MemberService memberService;
+    @Autowired
+    private BoardService boardService;
 
+    @Autowired
+    private PunishService punishService;
 
+    /**
+     * 로그인
+     * @param admin
+     * @param session
+     * @return
+     * @throws Exception
+     */
     @Override
     public AdminLoginVO adminLogin(AdminLoginRequestDTO admin, HttpSession session) throws Exception{
 
@@ -75,6 +92,39 @@ public class AdminServiceImpl implements AdminService {
             return true;
         } else {
             return false;
+        }
+    }
+
+
+    /**
+     * 전체 회원 조회
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public MemberListVO lookUpmembers() throws Exception{
+        try {
+            List<Member> members = memberService.findAllMembers();
+            List<MemberListDTO> resultData = new ArrayList<>();
+            for (Member member : members) {
+                resultData.add(MemberListDTO.builder()
+                                .email(member.getEmail())
+                                .memberName(member.getMemberName())
+                                .signupDate(member.getSignUpDate())
+                                .postCount(boardService.countPostById(member.getId()))  //글 개수
+                                .visitCount(member.getVisitCount())
+                                .privilege(member.convertPrivilege(member.getPrivilege()))
+                                .reportNum(punishService.countBytargetId(member.getId()))  //신고 수
+                                .authority(member.convertAuthority(member.getAuthority()))
+                        .build());
+            }
+            return MemberListVO.builder()
+                    .resultCode("success")
+                    .resultDataNum(members.size())
+                    .resultData(resultData)
+                    .build();
+        } catch (NullPointerException e) {
+            throw new Exception("failed : 조회할 수 있는 회원이 없습니다.");
         }
     }
 }
