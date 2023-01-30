40a8e10bf2bd846e5201bf6552ab2bbfb8c27df0
WONSEOKJE <nicholas4744@gmail.com>
2023-01-30 16:31:21 +0900

    feat: 멤버 엔티티 생성

---
+++ src/main/java/com/example/fieldpasserbe/entity/Member.java
@@ -0,0 +1,52 @@
+package com.example.fieldpasserbe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @Column(name = "MEMBER_ID")
+    private String memberId;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "MEMBERNAME")
+    private String memberName;
+
+    @Column(name = "PROFILE_IMG")
+    private String profileImg;
+
+    @Column(name = "PRIVILEGE", columnDefinition = "TINYINT", length = 1)
+    private int privilege;
+
+    @Column(name = "AUTHORITY", columnDefinition = "TINYINT", length = 1)
+    private int authority;
+
+    @CreationTimestamp
+    @Column(name = "SIGNUP_DATE")
+    private LocalDateTime signupDate = LocalDateTime.now();
+
+    @Column(name = "VISIT_COUNT")
+    private int visitCount;
+}
