bf306a1e502b821ecec9b5711ad7bf52e9ec7d9d
cocoiscat <demy176@naver.com>
2023-02-01 02:44:26 +0900

    feat: 멤버 전체 조회하는 기능 추가

--- src/main/java/com/example/fieldpasserbe/post/service/PostService.java
+++ src/main/java/com/example/fieldpasserbe/post/service/PostService.java
@@ -1,6 +1,11 @@
 package com.example.fieldpasserbe.post.service;
 
+import com.example.fieldpasserbe.post.dto.PostListResponseDto;
+import org.springframework.data.domain.Slice;
+
 public interface PostService {
 
     Long countPostById(int id);
+
+    Slice<PostListResponseDto> PostList(int page);
 }
--- src/main/java/com/example/fieldpasserbe/post/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/fieldpasserbe/post/service/impl/PostServiceImpl.java
@@ -2,17 +2,29 @@ package com.example.fieldpasserbe.post.service.impl;
 
 import com.example.fieldpasserbe.post.service.PostService;
 import com.example.fieldpasserbe.post.repository.PostRepositoryJPA;
+import com.example.fieldpasserbe.post.dto.PostListResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
-    private final PostRepositoryJPA boardRepository;
+    private final PostRepositoryJPA postRepository;
 
     @Override
     public Long countPostById(int id) {
-        return boardRepository.countPostById(id);
+        return postRepository.countPostById(id);
+    }
+
+    @Override
+    public Slice<PostListResponseDto> PostList(int page) {
+        PageRequest pageRequest = PageRequest.of(page - 1, 4, Sort.by(Sort.Direction.DESC, "registerDate"));
+
+        return postRepository.findDefaultAll(pageRequest)
+                .map(post -> new PostListResponseDto(post));
     }
 }
