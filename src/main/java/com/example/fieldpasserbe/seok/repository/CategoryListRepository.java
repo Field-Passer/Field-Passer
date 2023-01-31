f58413d9a9c7c67ead4c4317a5b346c8085cf94a
cocoiscat <demy176@naver.com>
2023-02-01 02:46:43 +0900

    remove: 패키지 통합 완료로 seok 패키지를 삭제함

--- src/main/java/com/example/fieldpasserbe/seok/repository/CategoryListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fieldpasserbe.seok.repository;
-
-import com.example.fieldpasserbe.seok.entity.CategoryList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryListRepository extends JpaRepository<CategoryList, Integer> {
-}
--- src/main/java/com/example/fieldpasserbe/seok/service/PostService.java
+++
@@ -1,8 +0,0 @@
-package com.example.fieldpasserbe.seok.service;
-
-import com.example.fieldpasserbe.seok.dto.PostListResponseDto;
-import org.springframework.data.domain.Slice;
-
-public interface PostService {
-    Slice<PostListResponseDto> PostList(int page);
-}
