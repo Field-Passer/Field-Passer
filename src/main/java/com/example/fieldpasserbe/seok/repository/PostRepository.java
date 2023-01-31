0c2610f06f310cbc47b32ffe18f826897943627a
WONSEOKJE <nicholas4744@gmail.com>
2023-01-31 17:33:40 +0900

    feat: 등록일자별 최신순 게시글 조회 구현

--- src/main/java/com/example/fieldpasserbe/seok/repository/PostRepository.java
+++ src/main/java/com/example/fieldpasserbe/seok/repository/PostRepository.java
@@ -1,7 +1,15 @@
 package com.example.fieldpasserbe.seok.repository;
 
 import com.example.fieldpasserbe.seok.entity.Post;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    @EntityGraph(attributePaths = {"member","categoryList","districtList","stadiumList"})
+    @Query("select p from Post p")
+    Slice<Post> findDefaultAll(PageRequest pageRequest);
 }
