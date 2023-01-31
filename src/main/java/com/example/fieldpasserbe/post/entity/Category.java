2a4a56bbd3fd6d414d8e4554c08c869cba67c6cb
cocoiscat <demy176@naver.com>
2023-02-01 01:35:48 +0900

    refactor: Category 엔티티 통합

--- src/main/java/com/example/fieldpasserbe/post/entity/Category.java
+++ src/main/java/com/example/fieldpasserbe/post/entity/Category.java
@@ -1,11 +1,14 @@
 package com.example.fieldpasserbe.post.entity;
 
+import com.example.fieldpasserbe.seok.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,8 +20,15 @@ public class Category {
 
     @Id
     @Column(name = "CATEGORY_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int categoryId;
 
     @Column(name = "CATEGORY")
     private String category;
+
+    @OneToMany(mappedBy = "categoryList")
+    private List<Post> postList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "categoryList")
+    private List<Stadium> stadiumList = new ArrayList<>();
 }
