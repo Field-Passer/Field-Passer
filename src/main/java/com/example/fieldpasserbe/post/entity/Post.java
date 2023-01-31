e1b571ca43dbc78fcac5cb2bbe79a881d7a9fe7a
cocoiscat <demy176@naver.com>
2023-02-01 01:31:14 +0900

    refactor : Post 엔티티 통합
    - 테이블 이름 BOARD -> POST
    - POST_ID 생성 규칙 적용
    - ID에 연관관계 적용
    - registerDate에 @CreationTimestamp
    - updateDate에 @UpdateTimestamp
    - 이용 시간 embedded변수 삭제
    - TransactionStatus enumerated 적용
    - viewCount, wishCount 초기화 함수 적용

--- src/main/java/com/example/fieldpasserbe/post/entity/Post.java
+++ src/main/java/com/example/fieldpasserbe/post/entity/Post.java
@@ -1,6 +1,10 @@
 package com.example.fieldpasserbe.post.entity;
 
+import com.example.fieldpasserbe.seok.entity.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -9,64 +13,77 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 @Entity
-@Table(name = "BOARD")
+@Table(name = "Post")
 public class Post {
-
     @Id
     @Column(name = "POST_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int postId;
 
-    @Column(name = "ID")
-    private int id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID")
+    private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "CATEGORY_ID")
-    private Category category;
+    private CategoryList categoryList;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "DISTRICT_ID")
-    private District district;
+    private DistrictList districtList;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "STADIUM_ID")
-    private Stadium stadium;
+    private StadiumList stadiumList;
 
-    @Column(name = "TITLE")
+    @Column(name = "TITLE", nullable = false)
     private String title;
 
-    @Column(name = "CONTENT")
+    @Column(name = "CONTENT", nullable = false)
     private String content;
 
+    @CreationTimestamp
     @Column(name = "REGISTER_DATE")
     private LocalDateTime registerDate;
 
+    @UpdateTimestamp
     @Column(name = "UPDATE_DATE")
     private LocalDateTime updateDate;
 
-    @Embedded
-    private UsageTime usageTime;
+    @Column(name = "DELETE_DATE")
+    private LocalDateTime deleteDate;
 
-    @Column(name = "END_TIME")
+    @Column(name = "START_TIME", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "END_TIME", nullable = false)
     private LocalDateTime endTime;
 
-    @Column(name = "IMAGE_URL")
-    private String imageURL;
+    @Column(name = "IMAGE_URL", nullable = false)
+    private String imageUrl;
 
     @Column(name = "TRANSACTION_STATUS")
-    private byte transactionStatus;
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus transactionStatus;
 
-    @Column(name = "PRICE")
+    @Column(name = "PRICE", nullable = false)
     private int price;
 
-    @Column(name = "VIEW_COUNT")
-    private int viewCount;
+    @Column(name = "VIEW_COUNT", nullable = false)
+    private Integer viewCount;
 
-    @Column(name = "WISH_COUNT")
-    private int wishCount;
+    @Column(name = "WISH_COUNT", nullable = false)
+    private Integer wishCount;
 
-    @Column(name = "BLIND")
+    @Column(name = "BLIND" , columnDefinition = "TINYINT(1) DEFAULT 0", length = 1)
     private byte blind;
 
-    @Column(name = "DELETE_CHECK")
+    @Column(name = "DELETE_CHECK", columnDefinition = "TINYINT(1) DEFAULT 0", length = 1)
     private byte deleteCheck;
+
+    @PrePersist
+    public void prePersist() {
+        this.viewCount = this.viewCount == null ? 0 : this.viewCount;
+        this.wishCount = this.wishCount == null ? 0 : this.wishCount;
+    }
 }
