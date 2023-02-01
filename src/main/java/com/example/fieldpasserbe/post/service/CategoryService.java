a3e9172258407d435d81bb48eca36751dba24afa
WONSEOKJE <nicholas4744@gmail.com>
2023-02-01 21:29:46 +0900

    feat: 구장 종류 리스트 조회 개발

---
+++ src/main/java/com/example/fieldpasserbe/post/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.example.fieldpasserbe.post.service;
+
+import com.example.fieldpasserbe.post.dto.CategoryResponseDto;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<CategoryResponseDto> category();
+}
