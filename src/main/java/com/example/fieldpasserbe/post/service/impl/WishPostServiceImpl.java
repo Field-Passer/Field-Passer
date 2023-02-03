20237931f624d5eebf49b861d702a34aae1758e1
WONSEOKJE <nicholas4744@gmail.com>
2023-02-03 18:09:15 +0900

    feat: 관심 글 등록시 위시카운트 증가 기능 구현

--- src/main/java/com/example/fieldpasserbe/post/service/impl/WishPostServiceImpl.java
+++ src/main/java/com/example/fieldpasserbe/post/service/impl/WishPostServiceImpl.java
@@ -1,20 +1,26 @@
 package com.example.fieldpasserbe.post.service.impl;
 
 import com.example.fieldpasserbe.post.dto.WishPostRequest;
+import com.example.fieldpasserbe.post.repository.PostRepositoryJPA;
 import com.example.fieldpasserbe.post.repository.WishPostRepository;
 import com.example.fieldpasserbe.post.service.WishPostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class WishPostServiceImpl implements WishPostService {
 
     private final WishPostRepository wishPostRepository;
+    private final PostRepositoryJPA postRepositoryJPA;
 
+    @Transactional
     @Override
     public String likePost(WishPostRequest wishPostRequest) {
         try {
+            int postId = wishPostRequest.getPostId();
+            postRepositoryJPA.updateWishCount(postId);
             wishPostRepository.save(wishPostRequest.toEntity());
         } catch (Exception e) {
             return "failed";
