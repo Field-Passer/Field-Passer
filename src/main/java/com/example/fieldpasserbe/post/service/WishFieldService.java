4c686a49ebfbfb8968709e4d6e0601aa4449804c
WONSEOKJE <nicholas4744@gmail.com>
2023-02-03 17:48:10 +0900

    feat: 관심 구장 등록 기능 구현

---
+++ src/main/java/com/example/fieldpasserbe/post/service/WishFieldService.java
@@ -0,0 +1,7 @@
+package com.example.fieldpasserbe.post.service;
+
+import com.example.fieldpasserbe.post.dto.WishFieldRequest;
+
+public interface WishFieldService {
+    String likeStadium(WishFieldRequest wishFieldRequest);
+}
