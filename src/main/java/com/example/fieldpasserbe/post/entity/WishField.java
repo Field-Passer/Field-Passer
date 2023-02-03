52d66f86e5b88663bfe33f396abbf21a27e4d3c3
WONSEOKJE <nicholas4744@gmail.com>
2023-02-03 17:28:51 +0900

    feat: WishField 엔티티 생성 및 설정

---
+++ src/main/java/com/example/fieldpasserbe/post/entity/WishField.java
@@ -0,0 +1,29 @@
+package com.example.fieldpasserbe.post.entity;
+
+import com.example.macpractice.dto.WishFieldId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@IdClass(WishFieldId.class)
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Wish_Field")
+public class WishField {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID")
+    private Member member;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STADIUM_ID")
+    private StadiumList stadiumList;
+}
