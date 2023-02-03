837ef5df15d95251e6c35e6b89a2815c62395922
WONSEOKJE <nicholas4744@gmail.com>
2023-02-03 17:29:32 +0900

    feat: WishPost 엔티티 생성 및 설정

---
+++ src/main/java/com/example/fieldpasserbe/post/entity/WishPost.java
@@ -0,0 +1,29 @@
+package com.example.fieldpasserbe.post.entity;
+
+import com.example.macpractice.dto.WishPostId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@IdClass(WishPostId.class)
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Wish_Post")
+public class WishPost {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID")
+    private Member member;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
+    private Post post;
+}
