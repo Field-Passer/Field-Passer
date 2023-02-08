20d3e548ebb44e83796f94726b936914cca09bd6
cocoiscat <demy176@naver.com>
2023-02-08 15:52:54 +0900

    feat: 글 블라인드 처리하기 DTO (개발 중)

---
+++ src/main/java/com/example/fieldpasserbe/admin/dto/BlindRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.fieldpasserbe.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class BlindRequestDTO {
+    private int postId;
+    private boolean blind;
+}
